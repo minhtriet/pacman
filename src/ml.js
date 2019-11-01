35b04f58815be0d07c0568f4c76fa328fd30a90a
minhtriet <mtriet.chau@gmail.com>
2019-11-01 16:56:09 +0100

    add eslint

--- src/ml.js
+++ src/ml.js
@@ -1,30 +1,28 @@
 // import * as tf from '@tensorflow/tfjs';
 
-/**const mobilenet = tf.loadLayersModel(
-      'https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json');*/
 
 // hit a coin, reward + 1
 // remove score
 // eat ghost
 // eat fruit
 // die
-let ob_next;
+const obNext = [];
 let reward;
-let log = [];
 // ob_next, reward, done, info = env.step(action)
 
-var logEnv = function(state, move_reward) {
-    let iterator = state.values();
+const logEnv = function(state, moveReward) {
+  if (state) {
+    const iterator = state.values();
     let substate;
     while (true) {
-        substate = iterator.next();
-        if (substate['done'] == true) {
-            break;
-        }
-        ob_next.push(...substate.value);
+      substate = iterator.next();
+      if (substate['done'] == true) {
+        break;
+      }
+      obNext.push(...substate.value);
     }
-    move_reward = reward;
+  }
+  reward = moveReward;
+};
 
-}
-
-export { logEnv }
+export {logEnv};
