68c56336d7fb34672c8f1b0fd2dfa0ea2f0195ee
minhtriet <mtriet.chau@gmail.com>
2019-11-03 11:39:56 +0100

    add function to create policy net

--- src/ml.js
+++ src/ml.js
@@ -12,7 +12,26 @@ let reward;
 
 const rl = function(ob, rw) {
   debugger;
-}
+};
+
+const createPolicyNetwork = function(hiddenLayerSizes) {
+  if (!Array.isArray(hiddenLayerSizes)) {
+    hiddenLayerSizes = [hiddenLayerSizes];
+  }
+  this.policyNet = tf.sequential();
+  hiddenLayerSizes.forEach((hiddenLayerSize, i) => {
+    this.policyNet.add(tf.layers.dense({
+      units: hiddenLayerSize,
+      activation: 'elu',
+      // `inputShape` is required only for the first layer.
+      inputShape: i === 0 ? [4] : undefined,
+    }));
+  });
+  // The last layer has only one unit. The single output number will be
+  // converted to a probability of selecting the leftward-force action.
+  this.policyNet.add(tf.layers.dense({units: 1}));
+};
+
 
 const logEnv = function(state, moveReward) {
   if (state) {
