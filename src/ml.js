60dd4ff465fef84312006ce868c30fc0d0590dc2
minhtriet <mtriet.chau@gmail.com>
2019-11-17 23:19:39 +0100

    unified model for logging

--- src/ml.js
+++ src/ml.js
@@ -8,9 +8,9 @@ const createPolicyNetwork = function(hiddenLayerSizes) {
   if (!Array.isArray(hiddenLayerSizes)) {
     hiddenLayerSizes = [hiddenLayerSizes];
   }
-  this.policyNet = tf.sequential();
+  let policyNet = tf.sequential();
   hiddenLayerSizes.forEach((hiddenLayerSize, i) => {
-    this.policyNet.add(tf.layers.dense({
+    policyNet.add(tf.layers.dense({
       units: hiddenLayerSize,
       activation: 'elu',
       // `inputShape` is required only for the first layer.
@@ -19,19 +19,21 @@ const createPolicyNetwork = function(hiddenLayerSizes) {
   });
   // The last layer has only one unit. The single output number will be
   // converted to a probability of selecting the leftward-force action.
-  this.policyNet.add(tf.layers.dense({units: 1}));
+  policyNet.add(tf.layers.dense({units: 1}));
 };
 
 
 const logEnv = function(state, moveReward) {
   if (state) {
+    let arr = Array(200).fill(0);
+    arr = arr.map(function(){return Array(200).fill(0)});
     // ghosts = [x][y][edible]
     let ghosts = tf.tensor(state.get('ghost_position').concat([state.get('ghost_edible')]).flat());
-    let user = tf.tensor([state.get('user_position')['x'], state.get('user_position')['y']);
+    let user = tf.tensor([state.get('user_position')['x'], state.get('user_position')['y']]);
     let map = tf.tensor(state.get('map'));
   }
   reward = moveReward;
-  
+
 };
 
 export {logEnv};
