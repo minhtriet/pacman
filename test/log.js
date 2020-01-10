50b304f45dbda8e20dd79ba5c269e10dc014e768
minhtriet <mtriet.chau@gmail.com>
2020-01-10 20:17:34 +0100

    add tests

--- test/log.js
+++ test/log.js
@@ -4,10 +4,10 @@ import {expect} from 'chai';
 describe('log', function () {
   it('expand map by padding zeroes in between', function () {
     const original = [[1, 2], [3, 4]];
-    const expanded = [[1, 0, 0, 2], [0, 0, 0, 0], [0, 0, 0, 0], [3, 0, 0, 4]];
+    const expanded = [[1, 0, 0, 0, 2], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [3, 0, 0 , 0, 4]];
 
-    const full_row = original.map(x => [...util.intersperse(x, Array(2).fill(0))]);
-    const actual = [...util.intersperse(full_row, Array(2).fill(Array(full_row[0].length).fill(0)))];
+    const full_row = original.map(x => [...util.intersperse(x, Array(3).fill(0))]);
+    const actual = [...util.intersperse(full_row, Array(3).fill(Array(full_row[0].length).fill(0)))];
     expect(actual).to.eql(expanded);
   });
 });
