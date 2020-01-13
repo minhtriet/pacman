5dc8c83c2457c18c0a1537ce7f8bd6e6ab2acb85
minhtriet <mtriet.chau@gmail.com>
2020-01-13 22:12:37 +0100

    refactor pointToCoord

--- src/util.js
+++ src/util.js
@@ -1,14 +1,5 @@
-const intersperse = function* (a, delim) {
-  let first = true;
-  for (const x of a) {
-    if (!first) {
-      for (const d of delim) {
-        yield d;
-      }
-    }
-    first = false;
-    yield x;
-  }
+const pointToCoord = function (x) {
+  return Math.round(x / 10);
 }
 
 export const WALL = 0;
@@ -19,4 +10,4 @@ export const PILL = 4;
 export const PLAYER = 5;
 export const INVULNERABLE_GHOST = 6;
 export const VULNERABLE_GHOST = 7;
-export { intersperse } 
+export { pointToCoord } 
